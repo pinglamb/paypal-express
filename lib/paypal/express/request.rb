8095a10293031a461440aa917b6df31a06ec8e0d
Gary Or <gary@beecrazy.com>
2012-03-16 15:28:39 +0800

    Allow specifying notify url in doExpressCheckout

--- lib/paypal/express/request.rb
+++ lib/paypal/express/request.rb
@@ -30,10 +30,11 @@ module Paypal
         Response.new response
       end
 
-      def checkout!(token, payer_id, payment_requests)
+      def checkout!(token, payer_id, payment_requests, notify_url)
         params = {
           :TOKEN => token,
-          :PAYERID => payer_id
+          :PAYERID => payer_id,
+          :PAYMENTREQUEST_0_NOTIFYURL => notify_url
         }
         Array(payment_requests).each_with_index do |payment_request, index|
           params.merge! payment_request.to_params(index)
