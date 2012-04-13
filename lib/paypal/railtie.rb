258b31a13091e42ff1cd65997bffa6662843b678
Gary Or <gary@beecrazy.com>
2012-04-13 11:31:49 +0800

    Allow specifying sandbox or production in yml

--- lib/paypal/railtie.rb
+++ lib/paypal/railtie.rb
@@ -2,6 +2,11 @@ module Paypal
   class Railtie < Rails::Railtie
     config.after_initialize do
       paypal_settings = YAML.load_file(File.join(Rails.root.to_s, 'config', 'paypal.yml'))[Rails.env || "development"]
+      if paypal_settings["environment"] == 0 # live
+        Paypal.sandbox = false
+      else
+        Paypal.sandbox = true
+      end
       Paypal.username = paypal_settings["username"]
       Paypal.password = paypal_settings["password"]
       Paypal.signature = paypal_settings["signature"]
